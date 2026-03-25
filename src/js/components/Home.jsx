05f51624e0f48176e75562d23ebac6d41a7fc590
Andy Andy <andylinjl@gmail.com>
2026-03-25 18:56:56 +0100

    Fetch Aplicado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,35 +1,96 @@
-import React,{useState}from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	const [Tarea,setTarea]=useState("")
-	const [Lista,setLista]=useState([])
+	const [Tarea, setTarea] = useState("");
+	const [Lista, setLista] = useState([]);
+
+	const getLista = () => {
+		fetch("https://playground.4geeks.com/todo/users/andylj01")
+			.then(res => res.json())
+			.then(data => {
+				setLista(data.todos || []);
+			})
+			.catch(Error => console.log(Error));
+	};
+
+	
+	const createUser = () => {
+		fetch("https://playground.4geeks.com/todo/users/andylj01", {
+			method: "POST"
+		})
+			.then(res => {
+				if (res.status !== 201 && res.status !== 200) {
+					return;
+				}
+			})
+			.catch(Error => console.log(Error));
+	};
+
+	const addTarea = (label) => {
+		fetch("https://playground.4geeks.com/todo/todos/andylj01", {
+			method: "POST",
+			body: JSON.stringify({
+				label: label,
+				done: false
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(() => getLista())
+			.catch(Error => console.log(Error));
+	};
+
+const deleteTarea = (id) => {
+	fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+		method: "DELETE"
+	})
+		.then(() => getLista())
+		.catch(Error => console.log(Error));
+};
+
+
+	useEffect(() => {
+		createUser();
+		getLista();
+	}, []);
 
 	return (
 		<div className="lista">
-          <h1 className="titulo">Mis Tareas:</h1>
-         <ul>
-			<li>
-			<input type="text"
-            onChange={(e)=>setTarea(e.target.value)}
-			value={Tarea}
-			onKeyDown={(e) => {
-				if (e.key === 'Enter') {
-					if (Tarea.trim() !== "") {
-						setLista([...Lista, Tarea]);
-						setTarea("");
-					}
-				}
-			}}
-			placeholder="Añade Tareas"></input></li>
-			{Lista.map((item,index)=>
-			<li key={index}>
-				{item} 
-				<i className="fa-solid fa-x" 
-				onClick={() => setLista(Lista.filter((_, i) => i !== index))}></i>
-			</li>)}
-		 </ul>
-	      <div className="numero">{Lista.length} Tasks</div>
+			<h1 className="titulo">Mis Tareas:</h1>
+
+			<ul>
+				<li>
+					<input
+						type="text"
+						onChange={(e) => setTarea(e.target.value)}
+						value={Tarea}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								if (Tarea.trim() !== "") {
+									addTarea(Tarea);
+									setTarea("");
+								}
+							}
+						}}
+						placeholder="Añade Tareas"
+					/>
+				</li>
+
+				{Lista.map((item) => (
+					<li key={item.id}>
+						{item.label}
+						<i
+							className="fa-solid fa-x"
+							onClick={() => deleteTarea(item.id)} 
+						></i>
+					</li>
+				))}
+			</ul>
+
+			<div className="numero">{Lista.length} Tasks</div>
+
 		</div>
 	);
 };
